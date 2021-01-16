12aad11eeae26cc63d3fcec25bf2639380be35e1
qingfeng.zhang <804875728@qq.com>
2021-01-16 18:38:38 +0800

    learn mut &

--- demo/hello-rust/src/main.rs
+++ demo/hello-rust/src/main.rs
@@ -1,9 +1,14 @@
+// 从标准库中引入输入/输出功能
+use std::io;
+
 fn main() {
     println!("Hello, world!");
-    // say_hello();
+
+    println!("Guess the number!");
+    let mut _guess = String::new(); // mut引入一个可变的变量
+
+    println!("Please input your guess.");
+    io::stdin().read_line(&mut _guess).expect("Failed to line"); // &符号表示这是一个引用， &mut表示这是一个可变的引用
 }
 
-fn say_hello() {
-    println!("hello Rust, my name is xiaofeng");
-}
 
