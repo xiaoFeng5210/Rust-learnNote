5549d53832e0f6a2803f3e68cdcbbab9e714ee9a
qingfeng.zhang <804875728@qq.com>
2021-01-21 22:20:02 +0800

    定义方法 impl

--- demo/hello-rust/src/main.rs
+++ demo/hello-rust/src/main.rs
@@ -2,15 +2,34 @@
 use std::io;
 
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
-    println!("Guess the number!");
-    let mut _guess = String::new(); // mut引入一个可变的变量
+    // println!("Guess the number!");
+    // let mut _guess = String::new(); // mut引入一个可变的变量
 
-    println!("Please input your guess.");
-    io::stdin().read_line(&mut _guess).expect("Failed to line"); // &符号表示这是一个引用， &mut表示这是一个可变的引用
+    // println!("Please input your guess.");
+    // io::stdin().read_line(&mut _guess).expect("Failed to line"); // &符号表示这是一个引用， &mut表示这是一个可变的引用
 
-    println!("You guessed: {}", _guess);
+    // println!("You guessed: {}", _guess);
+
+    let rect1 = Rectangle { width: 30, height: 50 };
+    println!("the rectangle is {}", rect1.area());
 }
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+
+
+
 
 
