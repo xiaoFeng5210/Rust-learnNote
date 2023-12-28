2beaf33c3cdcc95ea94448469b7242899e554b76
Eadweard <804875728@qq.com>
2023-12-28 13:26:47 +0800

    finish

--- demo/hello-rust/src/main.rs
+++ demo/hello-rust/src/main.rs
@@ -1,10 +1,15 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    // let stdout = stdout();
+    // let message = String::from("Hello fellow Rustaceans!");
+    // let width = message.chars().count();
+    // let mut writer = BufWriter::new(stdout.lock());
+    // say(&message, width, &mut writer).unwrap();
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("you guessed: {}", guess)
 }
