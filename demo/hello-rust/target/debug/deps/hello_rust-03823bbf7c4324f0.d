d5d9929f2da7122a1976d379b3cebba4a30e8aa9
Eadweard <804875728@qq.com>
2023-12-29 09:29:06 +0800

    fix

---
+++ demo/hello-rust/target/debug/deps/hello_rust-03823bbf7c4324f0.d
@@ -0,0 +1,5 @@
+/Users/xiaofeng/Documents/github/Rust-learnNote/demo/hello-rust/target/debug/deps/hello_rust-03823bbf7c4324f0.rmeta: src/main.rs
+
+/Users/xiaofeng/Documents/github/Rust-learnNote/demo/hello-rust/target/debug/deps/hello_rust-03823bbf7c4324f0.d: src/main.rs
+
+src/main.rs:
