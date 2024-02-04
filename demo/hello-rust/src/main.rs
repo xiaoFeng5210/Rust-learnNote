8b455e48d75da37fae7a2fbfc8a1331f26ad6b54
Eadweard <804875728@qq.com>
2024-02-04 15:17:27 +0800

    build

--- demo/hello-rust/src/main.rs
+++ demo/hello-rust/src/main.rs
@@ -3,7 +3,11 @@ use std::io;
 use rand::Rng;
 
 fn main() {
-    guess_number();
+    // guess_number();
+    // if_message();
+    // test_loop();
+    // while_loop();
+    for_loop();
 }
 
 fn guess_number() {
@@ -16,7 +20,47 @@ fn guess_number() {
     println!("you guessed: {}", guess)
 }
 
-// for循环
 fn for_loop() {
-    
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+        println!("the value is: {}", element);
+    }
+    for number in (1..=4).rev() {
+        println!("{}!", number);
+    }
 }
+
+fn if_message() {
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+}
+
+fn while_loop() {
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number -= 1;
+    }
+    println!("number < 0")
+}
+
+fn test_loop() {
+    let mut counter = 0;
+    let result = loop {
+        println!("again!");
+        counter += 1;
+        if counter == 10000 {
+            break counter * 2;
+        }
+    };
+    println!("the result is {}", counter);
+}
+
+// for循环
+// fn for_loop() {
+
+// }
