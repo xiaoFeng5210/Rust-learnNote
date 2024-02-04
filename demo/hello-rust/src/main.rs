2974c0df4d93a9a8d5d80e2e0777cec31b365a32
Eadweard <804875728@qq.com>
2024-02-04 15:23:54 +0800

    finish

--- demo/hello-rust/src/main.rs
+++ demo/hello-rust/src/main.rs
@@ -23,7 +23,7 @@ fn guess_number() {
 fn for_loop() {
     let a = [10, 20, 30, 40, 50];
     for element in a {
-        println!("the value is: {}", element);
+        println!("the value is: {}", element);                                                                                                                                                                                                                        
     }
     for number in (1..=4).rev() {
         println!("{}!", number);
